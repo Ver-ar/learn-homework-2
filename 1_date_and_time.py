6b6366836467eaad0ad7cbd598496f1605cce08a
Ver-ar <94222049+Ver-ar@users.noreply.github.com>
2021-12-07 10:23:01 +0300

    Time_done

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -1,3 +1,4 @@
+from datetime import date, datetime, timedelta
 """
 Домашнее задание №2
 
@@ -9,19 +10,27 @@
 """
 
 def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+   
+    dt_now = datetime.now()
+    dt_now.strftime('%d.%m.%Y %H^%M')
+    delta_yesterday = timedelta(days = 1)
+    dt_yesterday = dt_now - delta_yesterday
+    delta_months = timedelta(days = 30)
+    dt_months = dt_now - delta_months
+    delta_tommorow = timedelta(days = -1)
+    dt_tommorow = dt_now - delta_tommorow
+
+
+    print (f"Сегодня {dt_now.strftime('%d.%m.%Y %H:%M')}")
+    print (f"Вчера было {dt_yesterday.strftime('%d.%m.%Y %H:%M')}")
+    print (f"Месяц назад было {dt_months.strftime('%d.%m.%Y %H:%M')}")
+    print (f"Завтра будет {dt_tommorow.strftime('%d.%m.%Y %H:%M')}")
 
 
 def str_2_datetime(date_string):
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    
+    date_dt2 = datetime.strptime(date_string, '%m/%d/%y %H:%M:%S.%f')
+    print (date_dt2)
 
 if __name__ == "__main__":
     print_days()
