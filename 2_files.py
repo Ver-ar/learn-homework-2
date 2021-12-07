23296821780da9b36fef9b21bffaf92832c5f8c6
Ver-ar <94222049+Ver-ar@users.noreply.github.com>
2021-12-07 11:38:54 +0300

    Text_done

--- 2_files.py
+++ 2_files.py
@@ -10,13 +10,25 @@
 4. Замените точки в тексте на восклицательные знаки
 5. Сохраните результат в файл referat2.txt
 """
+import shutil
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+
+    with open ('referat.txt', "r", encoding='utf-8') as f:
+        shutil.copyfile("C:\\projects\\learn-homework-2\\referat.txt","C:\\projects\\learn-homework-2\\referat2.txt")
+
+    with open ('referat2.txt', "r", encoding='utf-8') as s:
+        content = s.read()
+
+    with open ('referat2.txt', "w+", encoding='utf-8') as s:    
+
+        str_content = str(content)
+        words = str_content.split()
+        str_content_2 = str_content.replace('.','!')
+        s.write(str_content_2)
+        print(str_content_2)
+        print (f'\nКоличество символов в строке - {len(str_content)}')
+        print(f'\nКоличество слов в тексте - {len(words)}')   
 
 if __name__ == "__main__":
     main()
